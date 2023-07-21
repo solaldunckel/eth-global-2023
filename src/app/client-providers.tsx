09fdcb55c3f84b60807e420ac7e61c7ec77d14f4
Solal Dunckel <solaldunckel@gmail.com>
2023-07-21 21:13:26 +0200

    fix nextjs error for rainbowkit

---
+++ src/app/client-providers.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum, zora } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, zora],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  projectId: "YOUR_PROJECT_ID",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+import type { FC } from "react";
+
+type ClientProvidersProps = {
+  children?: React.ReactNode;
+};
+
+const ClientProviders: FC<ClientProvidersProps> = ({ children }) => {
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>{children}</RainbowKitProvider>
+    </WagmiConfig>
+  );
+};
+
+export default ClientProviders;
